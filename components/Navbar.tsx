5035cc0aa96e78797317495e2515e8bcb56cf366
Pradhumna Pancholi <pnpancholi@protonmail.com>
2021-06-16 18:56:33 -0400

    added basic layout for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,17 @@
-import {Box, Flex} from '@chakra-ui/react'
+import {Box, Flex, Text, Button} from '@chakra-ui/react'
 import React from 'react'
 
 export default function Navbar() {
     return(
-        <Box h='120px' bg='#1E2230' borderRadius='18px'>
-            <Flex flexDirection='row' alignItems='center' justify='space-between' ml='42px' mr='42px' height='100%'>
-                <h1>Liquity</h1>
-                <Box d='flex' alignItems='center' justify='space-between'>
-                    <p>Borrow</p>
-                    <p>Deposit</p>
-                    <p>Stake</p>
+        <Box h='120px' w='100%' bg='#1E2230' borderRadius='18px'>
+            <Flex h='100%' alignItems='center' justifyContent='space-between'>
+                <Text fontSize='28px' color='white' ml='43px'>Liquity</Text>
+                <Box d='flex' alignItems='center' justify='space-between' height='100%'>
+                    <Text m='43px' fontSize='18px' color='white'>Borrow</Text>
+                    <Text m='43px' color='white'>Deposit</Text>
+                    <Text m='43px' color='white'>Stake</Text>
                 </Box>
+
             </Flex>
         </Box>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,6 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-
     </div>
   )
 }
