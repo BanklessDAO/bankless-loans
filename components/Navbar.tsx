1a27c5052440851c684d45b1e87eca92695aa795
Pradhumna Pancholi <pnpancholi@protonmail.com>
2021-06-18 16:24:06 -0400

    progress save navabr

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,41 @@
-import {Box, Flex, Text, Button} from '@chakra-ui/react'
+import {Box, Flex, Text, Stack, useDisclosure, Heading, Button} from '@chakra-ui/react'
+import {HamburgerIcon, CloseIcon} from '@chakra-ui/icons'
 import React from 'react'
 
 export default function Navbar() {
-    return(
-        <Box h='120px' w='100%' bg='#1E2230' borderRadius='18px'>
-            <Flex h='100%' alignItems='center' justifyContent='space-between'>
-                <Text fontSize='28px' color='white' ml='43px'>Liquity</Text>
-                <Box d='flex' alignItems='center' justify='space-between' height='100%'>
-                    <Text m='43px' fontSize='18px' color='white'>Borrow</Text>
-                    <Text m='43px' color='white'>Deposit</Text>
-                    <Text m='43px' color='white'>Stake</Text>
-                </Box>
 
+    const {isOpen, onOpen, onClose} = useDisclosure()
+
+    console.log('isOpen', isOpen)
+    const handleToggle = () => (isOpen ? onClose() : onOpen())
+
+    return( 
+        <Flex as='nav' h='120px' w='100%' bg='#1E2230' borderRadius='18px'
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        color='white'
+        >
+            <Flex h='100%' alignItems='center'>
+                <Text fontSize='28px' color='white' ml='43px'>Liquity</Text>
             </Flex>
-        </Box>
+                
+            <Box display={{ base: "block", md: "none" }} onClick={handleToggle}>
+                <HamburgerIcon color='white' mr='43px'/>
+            </Box>
+
+            <Stack
+                direction={{base:'column', md:'row'}}
+                display={{base: isOpen ? 'block' : 'none', md:'flex'}}
+                width={{ base: "full", md: "auto" }}
+                alignItems="center"
+                // mt={{ base: 4, md: 0 }}
+                bg='red'
+            >
+                <Text>Borrow</Text>
+                <Text>Deposit</Text>
+                <Text>Stake</Text>
+            </Stack>
+        </Flex>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import {ChakraProvider} from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <ChakraProvider>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </ChakraProvider>
   )
 }
 export default MyApp
