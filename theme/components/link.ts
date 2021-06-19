478a7ed0d59c4e4df591888221a83fe2bd5e2021
Zach Berwaldt <zberwaldt@tutamail.com>
2021-06-19 19:38:31 -0400

    fix reference to theme in _app.tsx, get link from figma into link override refined

--- theme/components/link.ts
+++ theme/components/link.ts
@@ -1,7 +1,22 @@
 const Link = {
-    baseStyle: {
-        mx: "20px"
-    },
+    baseStyle: ({ onDark } : { onDark?: boolean}) => ({
+        color: onDark ? "interactive.white" : 'black',
+        textDecoration: onDark ? "none": "underline",
+        fontWeight: 500,
+        fontSize: {
+            base: '1rem',
+        },
+        _hover: {
+            color: onDark ? "interactive.purple" : "interactive.darkPurple",
+            textDecoration: onDark ? "none" : "underline"
+        },
+        _active: {
+            color: onDark ? "background.blueBg" : "interactive.white"
+        },
+        _visited: {
+            color: onDark ? "interactive.grey": "black"
+        }
+    }),
     sizes: {},
     variants: {
         body: {
