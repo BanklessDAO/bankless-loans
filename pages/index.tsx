bed2eebb1c89c9fd76037a7629866af4c4f3fb48
Pradhumna Pancholi <pnpancholi@protonmail.com>
2021-06-18 22:50:55 -0400

    added wallet setup for Metamask and infura

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,36 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useWeb3Context } from 'web3-react'
+import React from 'react'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-    </div>
-  )
+
+  const web3Context = useWeb3Context()
+
+  React.useEffect(() => {
+    web3Context.setFirstValidConnector(['MetaMask', 'Infura'])
+  }, [])
+
+  if (!web3Context.active && !web3Context.error) {
+    console.log("something's wrong with web3Context")
+    return(
+      <p>Web3Context is broken</p>
+    )
+  } else if (web3Context.error) {
+    console.log('web3Context error')
+    return(
+      <p>Web3Context error</p>
+    )
+  } else{
+    console.log('web3Context', web3Context.account)
+    return(
+      <p>Web3Context works {web3Context.account}</p>
+    )
+  }
+
+  // return (
+  //   <div className={styles.container}>
+  //   </div>
+  // )
 }
