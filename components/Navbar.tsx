15b84d2d6f9df9a76e801594ecc65d5f7e75805e
Pradhumna Pancholi <pnpancholi@protonmail.com>
2021-06-20 18:25:37 -0400

    added basic wallet connection setup - metamask and walletconnect

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,10 @@
 import {Box, Flex, Text, Stack, useDisclosure, Button} from '@chakra-ui/react'
 import {HamburgerIcon, CloseIcon} from '@chakra-ui/icons'
 import React from 'react'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import {INFURA_ID} from '../constants/index'
+import { portis } from 'web3modal/dist/providers/connectors'
 
 export default function Navbar() {
 
@@ -8,6 +12,40 @@ export default function Navbar() {
 
     const handleToggle = () => (isOpen ? onClose() : onOpen())
     
+    let web3Modal : any
+
+    const providerOptions = {
+        injected: {
+            package: null
+        },
+        walletconnect: {
+            package: WalletConnectProvider,
+            options: {
+                infuraId: INFURA_ID // required
+            }
+        }
+        // portis : {
+        //     op
+            
+        // }
+    }
+
+    React.useEffect(() => {
+        web3Modal = new Web3Modal({
+            network: 'mainnet',
+            cacheProvider: true,
+            providerOptions
+        })
+    })
+    
+    const triggerWeb3Modal = () => {
+        web3Modal.connect()
+        console.log('web3Modal', web3Modal)
+
+    }
+
+    // const provider = web3Modal.connect()
+
     return( 
         <Flex as='nav' h='120px' w='100%' bg='#1E2230' borderRadius='18px'
         align="center"
@@ -42,7 +80,7 @@ export default function Navbar() {
                 <Text fontSize='18px' textAlign='center'>Borrow</Text>
                 <Text fontSize='18px' textAlign='center'>Deposit</Text>
                 <Text fontSize='18px' textAlign='center'>Stake</Text>
-                <Button variant="outlined">Connect Wallet</Button>
+                <Button variant="outlined" onClick={triggerWeb3Modal}>Connect Wallet</Button>
             </Stack>
         </Flex>
     )
