bbb4a0535da02afe7f8419a1dcbfb18f1213726f
Pradhumna Pancholi <pnpancholi@protonmail.com>
2021-06-22 22:47:39 -0400

    basic layout for custom web3 signin modal

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,8 @@ import Web3Modal from 'web3modal'
 import {INFURA_ID} from '../constants'
 import { InjectedConnector, FortmaticConnector, PortisConnector, WalletConnectConnector } from 'web3-react/dist/connectors'
 import { AuthereumConnector } from '@web3-react/authereum-connector'
+import Web3SignIn from './Web3SignIn'
+
 
 export default function Navbar() {
 
@@ -12,51 +14,51 @@ export default function Navbar() {
 
     const handleToggle = () => (isOpen ? onClose() : onOpen())
     
-    let web3Modal : any
+    // let web3Modal : any
 
-    const providerOptions = {
-        injected: {
-            package: null
-        },
-        walletconnect: {
-            package: WalletConnectConnector,
-            options: {
-                infuraId: INFURA_ID // required
-            }
-        },
-        portis: {
-            package: PortisConnector,
-            options: {
-                id: '47c91e55-8ecf-45f6-937d-9015bcf65b36'
-            }
-        },
-        fortmatic: { 
-            package: FortmaticConnector,
-            options: {
-              // test api key
-              key: "pk_test_43C2B609E8D78730"
-            }
-        },
-        authereum: {
-            package: AuthereumConnector
-        }
-    }
+    // const providerOptions = {
+    //     injected: {
+    //         package: null
+    //     },
+    //     walletconnect: {
+    //         package: WalletConnectConnector,
+    //         options: {
+    //             infuraId: INFURA_ID // required
+    //         }
+    //     },
+    //     portis: {
+    //         package: PortisConnector,
+    //         options: {
+    //             id: '47c91e55-8ecf-45f6-937d-9015bcf65b36'
+    //         }
+    //     },
+    //     fortmatic: { 
+    //         package: FortmaticConnector,
+    //         options: {
+    //           // test api key
+    //           key: "pk_test_43C2B609E8D78730"
+    //         }
+    //     },
+    //     authereum: {
+    //         package: AuthereumConnector
+    //     }
+    // }
 
-    React.useEffect(() => {
-        web3Modal = new Web3Modal({
-            network: 'mainnet',
-            cacheProvider: true,
-            providerOptions
-        })
-    })
+    // React.useEffect(() => {
+    //     web3Modal = new Web3Modal({
+    //         network: 'mainnet',
+    //         cacheProvider: true,
+    //         providerOptions
+    //     })
+    // })
     
-    const triggerWeb3Modal = (e: any) => {
-        // let provider = web3Modal.connect()
-        web3Modal.connect()
+    // const triggerWeb3Modal = (e: any) => {
+    //     // let provider = web3Modal.connect()
+    //     web3Modal.connect()
         
-        console.log('web3Modal', web3Modal)
+    //     console.log('web3Modal', web3Modal)
 
-    }
+    // }
 
     // const provider = web3Modal.connect()
 
@@ -94,7 +96,8 @@ export default function Navbar() {
                 <Text fontSize='18px' textAlign='center'>Borrow</Text>
                 <Text fontSize='18px' textAlign='center'>Deposit</Text>
                 <Text fontSize='18px' textAlign='center'>Stake</Text>
-                <Button variant="outlined" onClick={triggerWeb3Modal}>Connect Wallet</Button>
+                {/* <Button variant="outlined" onClick={triggerWeb3Modal}>Connect Wallet</Button> */}
+                <Web3SignIn />
             </Stack>
         </Flex>
     )
