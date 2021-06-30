82a6302851475bb929c1a48ef2299c5b759d5c8f
Zach Berwaldt <zberwaldt@tutamail.com>
2021-06-30 00:58:40 -0400

    add updated popover theme override

--- theme/components/popover.ts
+++ theme/components/popover.ts
@@ -1,10 +1,37 @@
 const Popover = {
+    parts: [
+        "pop", 
+        "content", 
+        "header", 
+        "body", 
+        "footer", 
+        "arrow"
+    ],
     baseStyle: {
-        bg: 'interative.dark',
-        color: 'white'
+        content: {
+            background: "interactive.dark",
+            minWidth: "393px",
+            lineHeight: "22px",
+            bg: "interactive.dark",
+            border: "none",
+            fontSize: "1rem"
+        },
+        header: {
+            padding: "14px 26px 0 20px",
+            border: "none",
+            color:"interactive.darkGrey"
+        },
+        body: {
+            padding: "14px 26px 14px 20px",
+            color: "white"
+        }
     },
-    sizes: {},
-    variants: {},
+    sizes: {
+
+    },
+    variants: {
+
+    },
     defaultProps: {}
 }
 
