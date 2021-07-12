efc56cc7274c7ac18652aa33baa6e87607de4267
Pradhumna Pancholi <pnpancholi@protonmail.com>
2021-07-12 19:54:58 -0400

    added minimalistic footer according to current data

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,21 @@
 import { 
-Box
+Box, Text, Flex, Stack, Spacer
 } from "@chakra-ui/react";
+import Link from 'next/link'
 
 export default function Footer() {
 
     return (
-        <Box p="5" my="3" bgGradient="linear(to-b, red.500, red.900)" textColor="white">
-            <h1>This is the Footer</h1>
+        <Box borderRadius='18px' height='150px'  bg='#1E2230' mt='10px' mb='1px' color='white' pt='50' pl='5%' pr='5%'>
+            <Flex>
+                <Text font='30px' fontWeight='800'>© Bankless Loans</Text>
+                <Spacer/>
+                {/* TODO: Need to work on those two pages to get links working */}
+                <Stack direction={['column', 'row']} spacing='35px'>
+                    <Link href='/'>Terms of use</Link>
+                    <Link href='/' >Privacy Policy</Link>
+                </Stack>
+            </Flex>
         </Box>        
     );
 }
