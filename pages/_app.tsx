8c27d0e4f0f2c1c1d6a0971ae9a00c247c3498a9
Pradhumna Pancholi <pnpancholi@protonmail.com>
2021-07-12 19:54:58 -0400

    added minimalistic footer according to current data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import {ChakraProvider} from '@chakra-ui/react'
-import {connectors} from '../utils/connectors'
+import connectors from '../utils/connectors'
 import { UseWalletProvider} from 'use-wallet'
 import theme from '../theme';
 
