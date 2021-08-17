bdb99653470922dff3c2b35749c51b48a90791bc
Birdman <geositta@protonmail.com>
2021-08-17 16:12:34 -0500

    Theme - Make StyledBox a Section component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,65 +3,77 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React from 'react'
 
-import { 
-  Box, 
-  Text, 
-  Flex, 
-  Heading, 
-  Button, 
-  Stack,   
+import {
+  Box,
+  Text,
+  Flex,
+  Heading,
+  Button,
+  Stack,
 } from "@chakra-ui/react";
 import Footer from '../components/Footer';
+import StyledBox from '../components/StyledBox';
 
 export default function Home() {
-  
+
   return (
     <Box p="5" borderRadius='18px'>
       {/* This is her0 section */}
       <Flex
-        bg="white" 
+        bg="white"
         h='310px'
-        direction="column" 
+        direction="column"
         borderRadius='18px'
         >
-        <Heading mt='105px' ml='120px'>
+        <Heading mt='105px' ml='120px' color='#6257DE'>
           Interest-free liquidity at your fingertips
         </Heading>
-        <Text mt='5px' ml='120px' lineHeight='22px'>
+        <Text mt='5px' ml='120px' lineHeight='22px' color='#6257DE'>
           Draw our LUSD stablecoin against ETH at 0% interest.
         </Text>
       </Flex>
       {/* ------------------------------------------------------ */}
-      
+
       {/* About Liquity section - this section cover TVL, LUSD, LQTY */}
       <Stack direction={["column", "row"]} my="3" spacing="3">
-        <Box 
-          h="500px" 
-          display="flex" 
-          flexDirection="column" 
-          justifyContent="space-around" 
-          p="5" 
-          w={["100%", "25%"]} 
+        <StyledBox
+          h="500px"
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-around"
+          p="5"
+          w={["100%", "25%"]}
           bg="white"
-          borderRadius='18px'
-        >     
-          <Heading textAlign="left" size="md" color='#6257DE' fontWeight='900' fontSize='30px' lineHeight='32px' letterSpacing='0.5'>
+        >
+          <Heading textAlign="left" size="md"
+            color='#6257DE'
+            fontWeight='900'
+            fontSize='30px'
+            lineHeight='32px'
+            letterSpacing='0.5'>
             Get rewards by depositing LUSD in stability pool
           </Heading>
-          <Text textAlign="center" size="sm">
+          <Text textAlign="center" size="sm"
+            color='#6257DE'>
             Earn LQTY rewards and liquidation gains for securing Liquity by providing LUSD to the Stability Pool.
           </Text>
-          <Button mt="3" size='md' width='200px' height='40px' variant='outline' ml='auto' mr='auto'>
+          <Button mt="3" size='md'
+            bg='#6257DE'
+            width='200px'
+            height='40px'
+            variant='outline'
+            ml='auto'
+            mr='auto'>
             Deposit LUSD
           </Button>
           <hr />
-          <Box borderRadius='18px'>
+          <Box borderRadius='18px' color='#6257DE'>
             <Text>Already deposited</Text>
             <Heading>100M LUSD</Heading>
             <Box ><img src=""></img></Box>
           </Box>
-        </Box>
-        <Box p="5" rounded="md" w={["100%", "50%"]} borderRadius='18px' color='#6257DE' bg='white'>
+        </StyledBox>
+        <StyledBox p="5" rounded="md" w={["100%", "50%"]} color='#6257DE' bg='white'>
           <Heading textAlign="center">Borrow against collateral</Heading>
           <Stack mt="3" direction={["column", "row"]}>
             <Box h="200px" w={["100%","60%"]} bg="palette.red">Image</Box>
@@ -70,53 +82,70 @@ export default function Home() {
               <Text>123.213444.</Text>
             </Stack>
           </Stack>
-          <Stack textAlign="center" mt="3" p="5" direction={["column", "row"]} borderRadius='18px'>
+          <Stack textAlign="center" mt="3" p="5"
+            direction={["column", "row"]}
+            borderRadius='18px'>
               <Text>some info here</Text>
               <Text>some other info there</Text>
               <Text>and some more</Text>
           </Stack>
-        </Box>
-        <Box           
-          h="500px" 
-          display="flex" 
-          flexDirection="column" 
-          justifyContent="space-around" 
-          p="5" 
-          w={["100%", "25%"]} 
-          bg="white"
-          borderRadius='18px'>
-          <Heading textAlign="left" size="md" color='#6257DE' fontWeight='900' fontSize='30px' lineHeight='32px' letterSpacing='0.5'>Earn fees by stacking LQTY</Heading>
+        </StyledBox>
+        <StyledBox
+          h="500px"
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-around"
+          p="5"
+          w={["100%", "25%"]}
+          bg="white">
+          <Heading textAlign="left" size="md"
+            color='#6257DE'
+            fontWeight='900'
+            fontSize='30px'
+            lineHeight='32px'
+            letterSpacing='0.5'>Earn fees by stacking LQTY</Heading>
           <Text mt='20px'>Stake LQTY to earn the fees from borrowing and redemptions</Text>
-          <Button size='md' width='200px' height='40px' variant='outline' ml='auto' mr='auto' mt='100px'>Stake LQTY</Button>
+          <Button size='md' width='200px'
+            height='40px'
+            variant='outline'
+            ml='auto'
+            mr='auto'
+            mt='100px'>Stake LQTY</Button>
           <hr />
           <Box borderRadius='18px'>
             <Text>Already deposited</Text>
             <Heading>100M LUSD</Heading>
             <Box ><img src=""></img></Box>
           </Box>
-        </Box>
+        </StyledBox>
       </Stack>
     {/* ------------------------------------------------------------------------------------- */}
     {/* How it works */}
       <Stack direction="column" p="5" color='#6257DE' bg='white' borderRadius='18px'>
         <Heading>How it works</Heading>
         <Box  display="flex" justifyItems="center" flexDirection="column">
-          <Stack justifyContent="center" direction={["column-reverse", "row"]} my="3" spacing="3">
+          <Stack justifyContent="center" direction={["column-reverse", "row"]}
+            my="3"
+            spacing="3">
             <Box><img src="#"></img></Box>
             <Box>Text</Box>
           </Stack>
-          <Stack justifyContent="center" direction={["column-reverse", "row"]} my="3" spacing="3">
+          <Stack justifyContent="center" direction={["column-reverse", "row"]}
+            my="3"
+            spacing="3">
             <Box><img src="#"></img></Box>
             <Box>Text</Box>
           </Stack>
-          <Stack justifyContent="center" direction={["column-reverse", "row"]} my="3" spacing="3">
+          <Stack justifyContent="center" direction={["column-reverse", "row"]}
+            my="3"
+            spacing="3">
             <Box><img src="#"></img></Box>
             <Box>Text</Box>
           </Stack>
         </Box>
-        
+
       </Stack>
-      <Box mt="3" p="5"  bg="white" borderRadius='18px'  color='#6257DE' >
+      <StyledBox mt="3" p="5"  bg="white" color='#6257DE' >
           <Heading size="md">Fees and charges</Heading>
           <Stack mt="3" justifyContent="space-around" direction={["column","row"]}>
             <Box>
@@ -132,7 +161,7 @@ export default function Home() {
               <Text fontSize="xs">descirption</Text>
             </Box>
           </Stack>
-        </Box>
+        </StyledBox>
     </Box>
   );
 }
