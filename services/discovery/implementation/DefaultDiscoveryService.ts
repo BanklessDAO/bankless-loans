b84667e8a0bda17a405b6f7cc8e1eb73ee5c0aee
Vlad Averin <vlad@orbitum.co>
2021-08-30 16:06:11 -0600

    Implement the discovery service with GraphQL API

---
+++ services/discovery/implementation/DefaultDiscoveryService.ts
@@ -0,0 +1,39 @@
+import DiscoveryService from '../interface/DiscoveryService'
+import LiquityState from '../../../models/LiquityState'
+import { 
+  GraphQLAPIClient, 
+  GLOBAL_STATE_QUERY 
+} from '../../../data/network/graph/interface/GraphQLAPIClient'
+
+class DefaultDiscoveryService implements DiscoveryService {
+  readonly graphQLAPIClient: GraphQLAPIClient
+
+  constructor (
+    graphQLAPIClient: GraphQLAPIClient
+  ) {
+    this.graphQLAPIClient = graphQLAPIClient
+  }
+
+  /**
+   * Get the most recent state of the protocol.
+   */
+  async getCurrentState(): Promise<LiquityState> {
+    // Get indexed on-chain data
+
+    var liquityState = this.graphQLAPIClient
+      .query(
+        GLOBAL_STATE_QUERY, 
+        {}, 
+        (mapper, response) => { 
+          return mapper.mapGlobalState(response.data.globals.currentSystemState)
+        }
+      )
+
+      console.log('Mapped data:')
+      console.log(liquityState)
+
+    return liquityState
+  }
+}
+
+export default DefaultDiscoveryService
