056a8127aa203696e5a9e4a74d418319e597ed29
Vlad Averin <vlad@orbitum.co>
2021-09-01 22:44:39 -0600

    Connect some of the Liquity state data to the homepage UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import {ChakraProvider} from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import { store } from '../state'
 import connectors from '../utils/connectors'
 import { UseWalletProvider} from 'use-wallet'
-import theme from '../theme';
 import customTheme from '../theme'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
@@ -12,9 +13,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     //This UseWalletProvider also helps in keeping a wallet's context at app level which can be accessed with useWallet hook anywhere in the app//
     <UseWalletProvider chainId={4} connectors={connectors}>
       <ChakraProvider theme={customTheme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
       </ChakraProvider>
     </UseWalletProvider>
   )
