fa0c15aa0b8d2fff87cc9a29ba20f3121c1d8828
Vlad Averin <vlad@orbitum.co>
2021-09-01 22:40:31 -0600

    Switch to hosted service endpoint for the subgraph; fix Liquity state mapping

--- data/network/graph/implementation/TheGraphAPIMapper.ts
+++ data/network/graph/implementation/TheGraphAPIMapper.ts
@@ -6,13 +6,15 @@ import GraphQLAPIMapper from "../interface/GraphQLAPIMapper"
  */
 class TheGraphAPIMapper implements GraphQLAPIMapper {
   mapGlobalState(rawState: any): LiquityState {
+    console.log('Mapping: ' + rawState)
+
     try {
       return new LiquityState(
-        rawState.totalCollateral,
-        rawState.totalCollateralRatio,
-        rawState.totalLQTYTokensStakes,
-        rawState.tokensInStabilityPool,
-        rawState.price
+        Number.parseFloat(rawState.totalCollateral),
+        Number.parseFloat(rawState.totalCollateralRatio),
+        Number.parseFloat(rawState.totalLQTYTokensStaked),
+        Number.parseFloat(rawState.tokensInStabilityPool),
+        Number.parseFloat(rawState.price)
       )
     } catch {
       throw new Error("Mapping failed")
