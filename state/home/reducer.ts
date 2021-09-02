23e3a2b8eec0b53d66380f0e13aa428cf2a14610
Vlad Averin <vlad@orbitum.co>
2021-09-01 22:43:37 -0600

    Implement some of the state hooks for the homepage

---
+++ state/home/reducer.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import LiquityState from '../../models/LiquityState'
+import { fetchHomeState } from './actions'
+
+export interface HomeState {
+  currentLiquityState: LiquityState | null
+}
+
+const initialState: HomeState = {
+  currentLiquityState: null
+} as HomeState
+
+const homeState = createSlice({
+  name: 'homeState',
+  initialState,
+  reducers: {
+    setLiquityState: (state, action: PayloadAction<LiquityState>) => {
+      state.currentLiquityState = action.payload
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchHomeState.fulfilled, (state, action) => {
+      state.currentLiquityState = action.payload
+    })
+  },
+})
+
+export const { setLiquityState } = homeState.actions
+
+export default homeState.reducer
