02fda9ba6a581675dfac6a9da9b1f9f2f8e3e000
Birdman <geositta@protonmail.com>
2021-09-22 16:37:06 -0500

    Add docs written by Prad

--- src/utils/connectors.ts
+++
@@ -1,7 +0,0 @@
-export const connectors = {
-  walletconnect: {rpcUrl: 'https://bridge.walletconnect.org'},
-  portis: { dAppId: 'eebbc594-d786-436c-803a-ad6b30b29efb' },
-  fortmatic: { apiKey: 'pk_live_4A7DF904F7AFC3DB'},
-}
-
-export default connectors
