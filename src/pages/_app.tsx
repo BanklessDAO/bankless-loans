1e6096fb1da8fccfea811392feee1224dfc2334f
Birdman <geositta@protonmail.com>
2021-09-25 13:24:59 -0500

    Remove connector import until local connector config can be setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,13 @@ import Layout from '../components/Layout'
 import {ChakraProvider} from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import { store } from '../state'
-import connectors from '../utils/connectors'
 import { UseWalletProvider} from 'use-wallet'
 import customTheme from '../theme'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     //This UseWalletProvider also helps in keeping a wallet's context at app level which can be accessed with useWallet hook anywhere in the app//
-    <UseWalletProvider chainId={4} connectors={connectors}>
+    <UseWalletProvider chainId={4}>
       <ChakraProvider theme={customTheme}>
         <Provider store={store}>
           <Layout>
