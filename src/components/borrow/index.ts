0bbd5c6a6095ccda378fceb73e63a7197fc6c655
Birdman <geositta@protonmail.com>
2021-12-01 15:24:56 -0600

    Add deps fixes

--- src/components/borrow/index.ts
+++ src/components/borrow/index.ts
@@ -1,9 +1,6 @@
-export { BorrowContext, default } from './create';
-
 /* Global typing for Borrow*/
 
 export type BorrowProps = {
     loan: string;
-    addLoan: (name: string) => void;
     children?: JSX.Element;
 };
