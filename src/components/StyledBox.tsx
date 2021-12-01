4e1ab68f61910df1d119f66769744c5a8d8561d1
Birdman <geositta@protonmail.com>
2021-12-01 15:24:56 -0600

    Add deps fixes

--- src/components/StyledBox.tsx
+++ src/components/StyledBox.tsx
@@ -2,16 +2,23 @@ import { Box, useStyleConfig } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 
 type Props = {
-    variant?: string,
-    children?: ReactNode,
-}
+    variant?: string;
+    children?: ReactNode;
+    color?: string;
+    bg?: string;
+    borderRadius?: string;
+    h?: string;
+    display?: string;
+    flexDirection?: string;
+    justifyContent?: string;
+    p?: string;
+    w?: string | string[];
+    rounded?: string;
+    mt?: string;
+};
 
-export default function StyledBox({variant, children, ...rest}: Props) {
-    const styles = useStyleConfig("Section", { variant });
+export default function StyledBox({ variant, children }: Props): JSX.Element {
+    const styles = useStyleConfig('Section', { variant });
 
-    return (
-        <Box __css={styles} {...rest}>
-            { children }
-        </Box>
-    )
+    return <Box __css={styles}>{children}</Box>;
 }
