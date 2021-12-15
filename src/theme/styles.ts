3047555e9f7870da14bfeb7a1e66fa6d078e5c99
abstrucked <mxhgraphics@gmail.com>
2021-12-15 21:57:51 +0100

    commit on abstrucked/UI/tooltips

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -1,12 +1,18 @@
+import { color } from '@chakra-ui/styled-system'
+
 const styles = {
     global: {
         html: {
-            fontSize: '18px'
+            fontSize: '18px',
         },
         body: {
-            bg: 'background.grey'
-        }
+            bg: 'background.grey',
+        },
+        tippyPopper: {
+            bg: '#ff0000',
+            color: 'ff0000',
+        },
     },
 }
 
-export default styles;
+export default styles
