8b7e914f5a7fddbea1d4ee2694b15bc1cb0f728e
Birdman <geositta@protonmail.com>
2022-01-26 11:25:11 -0600

    Use zero address for testing

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,76 +1,76 @@
-import { AddressZero } from "@ethersproject/constants";
-import { isAddress, getAddress } from "@ethersproject/address";
+import { AddressZero } from '@ethersproject/constants'
+import { isAddress, getAddress } from '@ethersproject/address'
 
 export type LiquityFrontendConfig = {
-  frontendTag?: string;
-  infuraApiKey?: string;
-  testnetOnly?: boolean;
-};
+    frontendTag?: string
+    infuraApiKey?: string
+    testnetOnly?: boolean
+}
 
 const defaultConfig: LiquityFrontendConfig = {
-  frontendTag: '0x4Aad6d8D45ba9a906B3DbcD9ee6F4d42e8D4a6B1',
-  infuraApiKey: 'a5d75a1abab84f9d8d37e7a67d3363fd',
-  testnetOnly: true
-};
+    frontendTag: '0x0000000000000000000000000000000000000000',
+    infuraApiKey: 'a5d75a1abab84f9d8d37e7a67d3363fd',
+    testnetOnly: true,
+}
 
 function hasKey<K extends string>(o: object, k: K): o is Record<K, unknown> {
-  return k in o;
+    return k in o
 }
 
 const parseConfig = (json: unknown): LiquityFrontendConfig => {
-  const config = { ...defaultConfig };
+    const config = { ...defaultConfig }
 
-  if (typeof json === "object" && json !== null) {
-    if (hasKey(json, "frontendTag") && json.frontendTag !== "") {
-      const { frontendTag } = json;
+    if (typeof json === 'object' && json !== null) {
+        if (hasKey(json, 'frontendTag') && json.frontendTag !== '') {
+            const { frontendTag } = json
 
-      if (typeof frontendTag === "string" && isAddress(frontendTag)) {
-        config.frontendTag = getAddress(frontendTag);
-      } else {
-        console.error("Malformed frontendTag:");
-        console.log(frontendTag);
-      }
+            if (typeof frontendTag === 'string' && isAddress(frontendTag)) {
+                config.frontendTag = getAddress(frontendTag)
+            } else {
+                console.error('Malformed frontendTag:')
+                console.log(frontendTag)
+            }
+        }
+
+        if (hasKey(json, 'infuraApiKey') && json.infuraApiKey !== '') {
+            const { infuraApiKey } = json
+
+            if (typeof infuraApiKey === 'string') {
+                config.infuraApiKey = infuraApiKey
+            } else {
+                console.error('Malformed infuraApiKey:')
+                console.log(infuraApiKey)
+            }
+        }
+
+        if (hasKey(json, 'testnetOnly')) {
+            const { testnetOnly } = json
+
+            if (typeof testnetOnly === 'boolean') {
+                config.testnetOnly = testnetOnly
+            } else {
+                console.error('Malformed testnetOnly:')
+                console.log(testnetOnly)
+            }
+        }
+    } else {
+        console.error('Malformed config:')
+        console.log(json)
     }
 
-    if (hasKey(json, "infuraApiKey") && json.infuraApiKey !== "") {
-      const { infuraApiKey } = json;
+    return config
+}
 
-      if (typeof infuraApiKey === "string") {
-        config.infuraApiKey = infuraApiKey;
-      } else {
-        console.error("Malformed infuraApiKey:");
-        console.log(infuraApiKey);
-      }
-    }
-
-    if (hasKey(json, "testnetOnly")) {
-      const { testnetOnly } = json;
-
-      if (typeof testnetOnly === "boolean") {
-        config.testnetOnly = testnetOnly;
-      } else {
-        console.error("Malformed testnetOnly:");
-        console.log(testnetOnly);
-      }
-    }
-  } else {
-    console.error("Malformed config:");
-    console.log(json);
-  }
-
-  return config;
-};
-
-let configPromise: Promise<LiquityFrontendConfig> | undefined = undefined;
+let configPromise: Promise<LiquityFrontendConfig> | undefined = undefined
 
 const fetchConfig = async () => {
-    return { ...defaultConfig };
-};
+    return { ...defaultConfig }
+}
 
 export const getConfig = (): Promise<LiquityFrontendConfig> => {
-  if (!configPromise) {
-    configPromise = fetchConfig();
-  }
+    if (!configPromise) {
+        configPromise = fetchConfig()
+    }
 
-  return configPromise;
-};
+    return configPromise
+}
