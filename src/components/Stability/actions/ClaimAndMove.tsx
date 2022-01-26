a8d4c7c679fecc4a4955e43d8c17b0430261fc94
Birdman <geositta@protonmail.com>
2022-01-26 07:55:34 -0600

    Introduce stability pool

---
+++ src/components/Stability/actions/ClaimAndMove.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button } from '@chakra-ui/react'
+import { useLiquity } from '../../../hooks/LiquityContext'
+import { useTransactionFunction } from '../../Transaction'
+
+type ClaimAndMoveProps = {
+    disabled?: boolean
+}
+
+export const ClaimAndMove: React.FC<ClaimAndMoveProps> = ({
+    disabled,
+    children,
+}) => {
+    const { liquity } = useLiquity()
+
+    const [sendTransaction] = useTransactionFunction(
+        'stability-deposit',
+        liquity.send.transferCollateralGainToTrove.bind(liquity.send)
+    )
+
+    return (
+        <Button
+            variant='outline'
+            sx={{ mt: 3, width: '100%' }}
+            onClick={sendTransaction}
+            disabled={disabled}
+        >
+            {children}
+        </Button>
+    )
+}
