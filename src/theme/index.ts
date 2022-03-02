8c3e34cabac2bd7e771b04a5d79fcd17f807dad9
Birdman <geositta@protonmail.com>
2022-03-02 12:58:00 -0600

    Test different approach with component styles

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -20,7 +20,7 @@ import colors from './colors'
 
 // Component style overrides.
 import Section from './components/section'
-import { Box } from '@chakra-ui/layout'
+import { Box, Container } from '@chakra-ui/layout'
 import Button from './components/button'
 import Link from './components/link'
 import Text from './components/text'
@@ -28,13 +28,30 @@ import Heading from './components/heading'
 import Popover from './components/popover'
 import Checkbox from './components/checkbox'
 
-const customTheme: any = extendTheme({
+const customTheme = extendTheme({
     colors,
     styles,
     fonts,
     components: {
         Section,
         Box,
+        Container,
+    },
+    layerStyles: {
+        baseStyle: {
+            width: '555px',
+            height: '622px',
+            bg: '#131313',
+            color: '#FFFFFF',
+            borderRadius: '31px',
+        },
+        otherStyle: {
+            width: '555px',
+            height: '622px',
+            bg: '#131313',
+            color: 'red',
+            borderRadius: '0px',
+        },
     },
     // components: {
     //     Section,
