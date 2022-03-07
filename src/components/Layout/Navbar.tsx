bcadfd4959dc19d6ef81f240864dd1014a4dd8d2
Birdman <geositta@protonmail.com>
2022-03-07 15:46:14 -0600

    Provisional nav bar changes

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -14,11 +14,11 @@ export default function Navbar() {
             h='130px'
             borderRadius='18px'
             w='100%'
-            bg='#131313'
+            bg='#363636'
             align='center'
-            justify='space-between'
-            wrap='wrap'
+            wrap='nowrap'
             color='white'
+            justify='space-between'
         >
             <Flex h='100%' alignItems='center'>
                 <Text fontSize='28px' color='white' ml='43px'>
@@ -33,23 +33,28 @@ export default function Navbar() {
                     <HamburgerIcon color='white' mr='43px' fontSize='25px' />
                 )}
             </Box>
-
-            <Stack
-                bg='#1E2230'
-                direction={{ base: 'column', md: 'row' }}
-                display={{ base: isOpen ? 'block' : 'none', md: 'flex' }}
-                width={{ base: '92%', md: 'auto' }}
-                height={{ base: 'full', lg: 'auto' }}
-                alignItems='center'
-                //this keeps it centered on mobile but allowing "space-between" on Navbar//
-                mr={{ base: 'auto', md: '43px' }}
-                ml={{ base: 'auto', md: '0px' }}
-                spacing={8}
-            >
-                <Link href='/'>Borrow</Link>
-                <Link href='/Pool'>Stability Pool</Link>
-                <Link href='/Stake'>Stake LQTY</Link>
-            </Stack>
+            <Flex h='100%' alignItems='center'>
+                <Stack
+                    bg='#131313'
+                    direction={{ base: 'column', md: 'row' }}
+                    display={{ base: isOpen ? 'block' : 'none', md: 'flex' }}
+                    width={{ base: '92%', md: 'auto' }}
+                    height={{ base: 'full', lg: 'auto' }}
+                    alignItems='center'
+                    //this keeps it centered on mobile but allowing "space-between" on Navbar//
+                    mr={{ base: 'auto', md: '43px' }}
+                    ml={{ base: 'auto', md: '0px' }}
+                    spacing={8}
+                    padding='8px'
+                    borderRadius='42px'
+                    border='1px solid'
+                >
+                    <Link href='/'>Borrow</Link>
+                    <Link href='/Pool'>Stability Pool</Link>
+                    <Link href='/Stake'>Stake LQTY</Link>
+                </Stack>
+            </Flex>
+            <Flex w='150px'></Flex>
         </Flex>
     )
 }
