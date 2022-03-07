07b12d44cb45bee40f5ab650646a0f5d60524abb
Birdman <geositta@protonmail.com>
2022-03-07 16:24:07 -0600

    Provisional stability pool and lqty staking layout changes

--- src/components/Stability/NoDeposit.tsx
+++ src/components/Stability/NoDeposit.tsx
@@ -13,31 +13,47 @@ export const NoDeposit: React.FC = () => {
     }, [dispatchEvent])
 
     return (
-        <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-            <Heading>
-                Stability Pool
-                <Flex sx={{ justifyContent: 'flex-end' }}>
-                    <RemainingLQTY />
-                </Flex>
-            </Heading>
-            <Box sx={{ p: [2, 3] }}>
-                <InfoMessage title='You have no LUSD in the Stability Pool.'>
-                    You can earn ETH and LQTY rewards by depositing LUSD.
-                </InfoMessage>
-
-                <Flex variant='layout.actions'>
-                    <Flex
-                        sx={{
-                            justifyContent: 'flex-start',
-                            flex: 1,
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Yield />
+        <Flex
+            w='555px'
+            height='432px'
+            alignItems='center'
+            justifyContent='center'
+        >
+            <Box
+                w='100%'
+                h='100%'
+                maxW='md'
+                borderWidth={1}
+                borderRadius='31px'
+                overflow='hidden'
+                bg='#131313'
+                color='#FFFFFF'
+            >
+                <Heading>
+                    Stability Pool
+                    <Flex sx={{ justifyContent: 'flex-end' }}>
+                        <RemainingLQTY />
                     </Flex>
-                    <Button onClick={handleOpenTrove}>Deposit</Button>
-                </Flex>
+                </Heading>
+                <Box sx={{ p: [2, 3] }}>
+                    <InfoMessage title='You have no LUSD in the Stability Pool.'>
+                        You can earn ETH and LQTY rewards by depositing LUSD.
+                    </InfoMessage>
+
+                    <Flex variant='layout.actions'>
+                        <Flex
+                            sx={{
+                                justifyContent: 'flex-start',
+                                flex: 1,
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Yield />
+                        </Flex>
+                        <Button onClick={handleOpenTrove}>Deposit</Button>
+                    </Flex>
+                </Box>
             </Box>
-        </Box>
+        </Flex>
     )
 }
