fcd8822e3f8d511a1bc7dde1a3c585c056c6fab4
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-16 23:44:54 +0000

    STYLE (#92) : adds max-width bounds in the main layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,20 @@
 import { Fragment } from 'react'
+import styled from '@emotion/styled'
 import Navbar from './Navbar'
+
+const StyledMain = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 1440px;
+    margin: 0 auto;
+`
 
 export default function Layout({ children }: any) {
     return (
-        <Fragment>
+        <StyledMain>
             <Navbar />
             {children}
-        </Fragment>
+        </StyledMain>
     )
 }
