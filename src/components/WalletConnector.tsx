968c6bbcbf90cda24c88ace96935ef2e2fa64a91
Birdman <geositta@protonmail.com>
2022-03-17 11:17:06 -0500

    Create separate file for wallet connection reducer

--- src/components/WalletConnector.tsx
+++ src/components/WalletConnector.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer, useState } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { AbstractConnector } from '@web3-react/abstract-connector'
+import { walletReducer } from '../hooks/walletReducer'
 import { Button, Text, Flex, Link, Box } from '@chakra-ui/react'
 import { injectedConnector } from '../connectors/injectedConnector'
 import { useAuthorizedConnection } from '../hooks/useAuthorizedConnection'
@@ -25,79 +26,6 @@ interface MaybeHasMetaMask {
     }
 }
 
-type ConnectionState =
-    | { type: 'inactive' }
-    | {
-          type:
-              | 'activating'
-              | 'active'
-              | 'rejectedByUser'
-              | 'alreadyPending'
-              | 'failed'
-          connector: AbstractConnector
-      }
-
-type ConnectionAction =
-    | { type: 'startActivating'; connector: AbstractConnector }
-    | { type: 'fail'; error: Error }
-    | { type: 'finishActivating' | 'retry' | 'cancel' | 'deactivate' }
-
-const connectionReducer: React.Reducer<ConnectionState, ConnectionAction> = (
-    state,
-    action
-) => {
-    switch (action.type) {
-        case 'startActivating':
-            return {
-                type: 'activating',
-                connector: action.connector,
-            }
-        case 'finishActivating':
-            return {
-                type: 'active',
-                connector:
-                    state.type === 'inactive'
-                        ? injectedConnector
-                        : state.connector,
-            }
-        case 'fail':
-            if (state.type !== 'inactive') {
-                return {
-                    type: action.error.message.match(/user rejected/i)
-                        ? 'rejectedByUser'
-                        : action.error.message.match(/already pending/i)
-                        ? 'alreadyPending'
-                        : 'failed',
-                    connector: state.connector,
-                }
-            }
-            break
-        case 'retry':
-            if (state.type !== 'inactive') {
-                return {
-                    type: 'activating',
-                    connector: state.connector,
-                }
-            }
-            break
-        case 'cancel':
-            return {
-                type: 'inactive',
-            }
-        case 'deactivate':
-            return {
-                type: 'inactive',
-            }
-    }
-
-    console.warn('Ignoring connectionReducer action:')
-    console.log(action)
-    console.log('  in state:')
-    console.log(state)
-
-    return state
-}
-
 type WalletConnectorProps = {
     loader?: React.ReactNode
 }
@@ -108,7 +36,7 @@ export const WalletConnector: React.FC<WalletConnectorProps> = ({
 }) => {
     const { activate, deactivate, active, error } = useWeb3React<unknown>()
     const triedAuthorizedConnection = useAuthorizedConnection()
-    const [connectionState, dispatch] = useReducer(connectionReducer, {
+    const [connectionState, dispatch] = useReducer(walletReducer, {
         type: 'inactive',
     })
     const [isMetaMask, setIsMetaMask] = useState(false)
