8ec4526afc996ec0b37551d69ca834184ca9aeda
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-17 00:34:00 +0000

    FEAT (#92) : updates the Navbar component
    Removed older drawer for now - design are slightly different
    
    Adds in all the new components and styling

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,55 +1,27 @@
-import { Box, Flex, Text, Stack, useDisclosure, Button } from '@chakra-ui/react'
-import Link from 'next/link'
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 import React from 'react'
+import styled from '@emotion/styled'
+import Logo from 'components/Logo'
+import NavbarLinks from 'components/NavbarLinks'
+import NavbarWallet from 'components/NavbarWallet'
+
+const StyledNav = styled.nav`
+    height: 130px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 5vw;
+    & > * {
+        flex: 0 0 auto;
+    }
+`
 
 export default function Navbar() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
-    const handleToggle = () => (isOpen ? onClose() : onOpen())
-
     return (
-        <Flex
-            as='nav'
-            h='130px'
-            borderRadius='18px'
-            w='100%'
-            bg='#1E2230'
-            align='center'
-            justify='space-between'
-            wrap='wrap'
-            color='white'
-        >
-            <Flex h='100%' alignItems='center'>
-                <Text fontSize='28px' color='white' ml='43px'>
-                    Bankless Loans
-                </Text>
-            </Flex>
-
-            <Box display={{ base: 'block', md: 'none' }} onClick={handleToggle}>
-                {isOpen ? (
-                    <CloseIcon color='white' mr='43px' fontSize='15px' />
-                ) : (
-                    <HamburgerIcon color='white' mr='43px' fontSize='25px' />
-                )}
-            </Box>
-
-            <Stack
-                bg='#1E2230'
-                direction={{ base: 'column', md: 'row' }}
-                display={{ base: isOpen ? 'block' : 'none', md: 'flex' }}
-                width={{ base: '92%', md: 'auto' }}
-                height={{ base: 'full', lg: 'auto' }}
-                alignItems='center'
-                //this keeps it centered on mobile but allowing "space-between" on Navbar//
-                mr={{ base: 'auto', md: '43px' }}
-                ml={{ base: 'auto', md: '0px' }}
-                spacing={8}
-            >
-                <Link href='/'>Borrow</Link>
-                <Link href='/Pool'>Stability Pool</Link>
-                <Link href='/Stake'>Stake LQTY</Link>
-            </Stack>
-        </Flex>
+        <StyledNav>
+            <Logo />
+            <NavbarLinks />
+            <NavbarWallet />
+        </StyledNav>
     )
 }
