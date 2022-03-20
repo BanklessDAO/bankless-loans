b3620e3d94f91659a3faf7395a6cd53c910bc7ca
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-20 15:24:28 +0000

    REFACTOR (Layout): refactors to use Chakra

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,19 +1,13 @@
-import { Fragment } from 'react'
-import styled from '@emotion/styled'
+import { Container, VStack } from '@chakra-ui/react'
 import Navbar from './Navbar'
 
-const StyledMain = styled.main`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    max-width: 1440px;
-    margin: 0 auto;
-`
 export default function Layout({ children }: JSX.ElementChildrenAttribute) {
     return (
-        <StyledMain>
-            <Navbar />
-            {children}
-        </StyledMain>
+        <Container maxW='container.xl'>
+            <VStack align='center'>
+                <Navbar />
+                {children}
+            </VStack>
+        </Container>
     )
 }
