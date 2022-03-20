ffbc2062c676f56262c37f4befa4e4f5e0cf142c
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-20 23:53:33 +0000

    FEAT (NavbarWallet) : adds walletAddress prop and renders ellipsis

--- src/components/NavbarWallet.tsx
+++ src/components/NavbarWallet.tsx
@@ -5,21 +5,26 @@ import walletImage from '../../public/walletIcon.jpg'
 
 const ChakraNextImage = chakra(Image)
 
-export default function NavbarWallet() {
+type Props = {
+    walletAddress?: string
+}
+
+export default function NavbarWallet({
+    walletAddress = '0x820C24163f64AcB6AA6fa094B3F2444A6Ab1bCe9',
+}: Props) {
     return (
         <HStack
             bg='#131313'
-            spacing={2}
             p={2}
             borderRadius='18px'
             alignItems='center'
             border='0.8px solid #7d7d7d'
             mr={1}
         >
-            <Text as='p' ml={2}>
-                0xd4eb...534C
+            <Text as='p' isTruncated noOfLines={0} maxWidth='126px'>
+                {walletAddress}
             </Text>
-            <Box as='figure' h='28px' w='28px'>
+            <Box as='figure' h='28px' w='28px' ml={2}>
                 <ChakraNextImage
                     src={walletImage}
                     alt='wallet image'
