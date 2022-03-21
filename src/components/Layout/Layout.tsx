30bb06067feca933f9a7bb5698868bb3025623a9
Birdman <geositta@protonmail.com>
2022-03-21 12:28:25 -0500

    Cleanup Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,11 @@ import { Container, VStack } from '@chakra-ui/react'
 import Navbar from './Navbar'
 import NavbarMobile from '../NavbarMobile'
 
-export default function Layout({ children }: JSX.ElementChildrenAttribute) {
+type LayoutProps = {
+    children?: React.ReactNode
+}
+
+export const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
         <Container maxW='container.xl' p={0}>
             <VStack align='center' as='main' position='relative' height='100vh'>
