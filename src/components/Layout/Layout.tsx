039da04960889179d7084a5ad8b630ee38da73c5
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-21 00:36:50 +0000

    FEAT (Layout+Stake): updates the layout to improve responsiveness
    Removes padding from container
    
    Fixes the height to 100vh
    
    Updates the heigh on the Stake page
    
    Integrates the new NavbarMobile to the Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { Container, VStack } from '@chakra-ui/react'
 import Navbar from './Navbar'
+import NavbarMobile from '../NavbarMobile'
 
 export default function Layout({ children }: JSX.ElementChildrenAttribute) {
     return (
-        <Container maxW='container.xl'>
-            <VStack align='center'>
+        <Container maxW='container.xl' p={0}>
+            <VStack align='center' as='main' position='relative' height='100vh'>
                 <Navbar />
                 {children}
+                <NavbarMobile />
             </VStack>
         </Container>
     )
