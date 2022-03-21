9208e5c0193347079fec2be10d8a1ac0c6d1887f
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-21 00:38:21 +0000

    FEAT (NavbarMobile) : adds new Nav component designed to appear only on mobile

---
+++ src/components/NavbarMobile.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Box, Container } from '@chakra-ui/react'
+import NavbarWallet from './NavbarWallet'
+
+export default function NavbarMobile() {
+    return (
+        <Container
+            maxW='container.xl'
+            p={0}
+            w='100%'
+            centerContent
+            display={['block', 'block', 'none']}
+        >
+            <Box
+                bg='#101010'
+                h='92px'
+                position='absolute'
+                bottom={0}
+                w='100%'
+                borderRadius='30px 30px 0px 0px'
+                boxShadow='0px 1px 18px #343434'
+                display='flex'
+                alignItems='center'
+                justifyContent='center'
+            >
+                <NavbarWallet />
+            </Box>
+        </Container>
+    )
+}
