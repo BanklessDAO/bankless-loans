c2148348e222f757218ded0f0295c6984def68fb
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-23 23:21:11 +0000

    REFACTOR (pages) : removed unnecessary styles from /index and /dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,13 +2,7 @@ import { Grid, Box } from '@chakra-ui/react'
 import { Trove } from '../components/Trove/Trove'
 
 const Dashboard = (): JSX.Element => {
-    return (
-        <Grid display='flex' flexDirection={{ base: 'column', lg: 'row' }}>
-            <Box>
-                <Trove />
-            </Box>
-        </Grid>
-    )
+    return <Trove />
 }
 
 export default Dashboard
