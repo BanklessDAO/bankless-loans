35b9cb664a2de41b8db6838dba07bb813d1ebdc9
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-23 23:12:54 +0000

    REFACTOR (CardBase) : implements CardBase on Staking pages NoStake and StakingEditor

--- src/components/Staking/NoStake.tsx
+++ src/components/Staking/NoStake.tsx
@@ -1,43 +1,27 @@
 import { Heading, Box, Flex, Button } from '@chakra-ui/react'
 import { GT } from '../../strings'
 import { InfoMessage } from '../InfoMessage'
+import { CardBase } from '../Layout/CardBase'
 import { useStakingView } from './context/StakingViewContext'
 
 export const NoStake: React.FC = () => {
     const { dispatch } = useStakingView()
 
     return (
-        <Flex
-            w='555px'
-            height='432px'
-            alignItems='center'
-            justifyContent='center'
-        >
-            <Box
-                w='100%'
-                h='100%'
-                maxW='md'
-                borderWidth={1}
-                borderRadius='31px'
-                overflow='hidden'
-                bg='#131313'
-                color='#FFFFFF'
-            >
-                <Heading>Staking</Heading>
-                <Box sx={{ p: [2, 3] }}>
-                    <InfoMessage title={`You have not staked ${GT} yet.`}>
-                        Stake {GT} to earn a share of borrowing and redemption
-                        fees.
-                    </InfoMessage>
-                    <Flex variant='layout.actions'>
-                        <Button
-                            onClick={() => dispatch({ type: 'startAdjusting' })}
-                        >
-                            Start staking
-                        </Button>
-                    </Flex>
-                </Box>
+        <CardBase>
+            <Heading>Staking</Heading>
+            <Box sx={{ p: [2, 3] }}>
+                <InfoMessage title={`You have not staked ${GT} yet.`}>
+                    Stake {GT} to earn a share of borrowing and redemption fees.
+                </InfoMessage>
+                <Flex variant='layout.actions'>
+                    <Button
+                        onClick={() => dispatch({ type: 'startAdjusting' })}
+                    >
+                        Start staking
+                    </Button>
+                </Flex>
             </Box>
-        </Flex>
+        </CardBase>
     )
 }
