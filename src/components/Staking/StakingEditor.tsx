0f00a3d12ebff39f4e510d05302adc90dd929620
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-23 23:12:54 +0000

    REFACTOR (CardBase) : implements CardBase on Staking pages NoStake and StakingEditor

--- src/components/Staking/StakingEditor.tsx
+++ src/components/Staking/StakingEditor.tsx
@@ -12,6 +12,7 @@ import { COIN, GT } from '../../strings'
 import { Icon } from '../Icon'
 import { EditableRow, StaticRow } from '../Trove/Editor'
 import { LoadingOverlay } from '../LoadingOverlay'
+import { CardBase } from '../Layout/CardBase'
 import { useStakingView } from './context/StakingViewContext'
 
 const select = ({ lqtyBalance, totalStakedLQTY }: LiquityStoreState) => ({
@@ -54,98 +55,76 @@ export const StakingEditor: React.FC<StakingEditorProps> = ({
         Difference.between(newPoolShare, originalPoolShare).nonZero
 
     return (
-        <Flex
-            w='555px'
-            height='622px'
-            alignItems='center'
-            justifyContent='center'
-        >
-            <Box
-                w='100%'
-                h='100%'
-                maxW='md'
-                borderRadius='31px'
-                overflow='hidden'
-                padding={[10, 34, 34, 5]}
-                bg='#131313'
-                color='#FFFFFF'
-            >
-                <Heading>
-                    {title}
-                    {edited && !changePending && (
-                        <Button
-                            variant='titleIcon'
-                            sx={{ ':enabled:hover': { color: 'danger' } }}
-                            onClick={() => dispatch({ type: 'revert' })}
-                        >
-                            <Icon name='history' size='lg' />
-                        </Button>
-                    )}
-                </Heading>
-                <Box sx={{ p: [2, 3] }}>
-                    <EditableRow
-                        label='Stake'
-                        inputID='stake-lqty'
-                        amount={editedLQTY.prettify()}
-                        maxAmount={maxAmount.toString()}
-                        maxedOut={maxedOut}
-                        unit={GT}
-                        {...{ editingState }}
-                        editedAmount={editedLQTY.toString(2)}
-                        setEditedAmount={newValue =>
-                            dispatch({ type: 'setStake', newValue })
-                        }
+        <CardBase>
+            <Heading>
+                {title}
+                {edited && !changePending && (
+                    <Button
+                        variant='titleIcon'
+                        sx={{ ':enabled:hover': { color: 'danger' } }}
+                        onClick={() => dispatch({ type: 'revert' })}
+                    >
+                        <Icon name='history' size='lg' />
+                    </Button>
+                )}
+            </Heading>
+            <Box sx={{ p: [2, 3] }}>
+                <EditableRow
+                    label='Stake'
+                    inputID='stake-lqty'
+                    amount={editedLQTY.prettify()}
+                    maxAmount={maxAmount.toString()}
+                    maxedOut={maxedOut}
+                    unit={GT}
+                    {...{ editingState }}
+                    editedAmount={editedLQTY.toString(2)}
+                    setEditedAmount={newValue =>
+                        dispatch({ type: 'setStake', newValue })
+                    }
+                />
+                {newPoolShare.infinite ? (
+                    <StaticRow
+                        label='Pool share'
+                        inputID='stake-share'
+                        amount='N/A'
                     />
-                    {newPoolShare.infinite ? (
+                ) : (
+                    <StaticRow
+                        label='Pool share'
+                        inputID='stake-share'
+                        amount={newPoolShare.prettify(4)}
+                        pendingAmount={poolShareChange?.prettify(4).concat('%')}
+                        pendingColor={
+                            poolShareChange?.positive ? 'success' : 'danger'
+                        }
+                        unit='%'
+                    />
+                )}
+                {!originalStake.isEmpty && (
+                    <>
                         <StaticRow
-                            label='Pool share'
-                            inputID='stake-share'
-                            amount='N/A'
-                        />
-                    ) : (
-                        <StaticRow
-                            label='Pool share'
-                            inputID='stake-share'
-                            amount={newPoolShare.prettify(4)}
-                            pendingAmount={poolShareChange
-                                ?.prettify(4)
-                                .concat('%')}
-                            pendingColor={
-                                poolShareChange?.positive ? 'success' : 'danger'
+                            label='Redemption gain'
+                            inputID='stake-gain-eth'
+                            amount={originalStake.collateralGain.prettify(4)}
+                            color={
+                                originalStake.collateralGain.nonZero &&
+                                'success'
                             }
-                            unit='%'
+                            unit='ETH'
                         />
-                    )}
-                    {!originalStake.isEmpty && (
-                        <>
-                            <StaticRow
-                                label='Redemption gain'
-                                inputID='stake-gain-eth'
-                                amount={originalStake.collateralGain.prettify(
-                                    4
-                                )}
-                                color={
-                                    originalStake.collateralGain.nonZero &&
-                                    'success'
-                                }
-                                unit='ETH'
-                            />
 
-                            <StaticRow
-                                label='Issuance gain'
-                                inputID='stake-gain-lusd'
-                                amount={originalStake.lusdGain.prettify()}
-                                color={
-                                    originalStake.lusdGain.nonZero && 'success'
-                                }
-                                unit={COIN}
-                            />
-                        </>
-                    )}
-                    {children}
-                </Box>
-                {changePending && <LoadingOverlay />}
+                        <StaticRow
+                            label='Issuance gain'
+                            inputID='stake-gain-lusd'
+                            amount={originalStake.lusdGain.prettify()}
+                            color={originalStake.lusdGain.nonZero && 'success'}
+                            unit={COIN}
+                        />
+                    </>
+                )}
+                {children}
             </Box>
-        </Flex>
+            {changePending && <LoadingOverlay />}
+        </CardBase>
     )
 }
