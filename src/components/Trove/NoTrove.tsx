93963d5c628aaa53b6c6d62e526f47afc8622122
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-03-25 01:47:04 +0000

    FEAT (trove/no-trove) : adds generic card component, increasing visual consistency

--- src/components/Trove/NoTrove.tsx
+++ src/components/Trove/NoTrove.tsx
@@ -2,6 +2,7 @@ import React, { useCallback } from 'react'
 import { Heading, Box, Flex, Button } from '@chakra-ui/react'
 import { InfoMessage } from '../InfoMessage'
 import { useTroveView } from './context/TroveViewContext'
+import { CardBase } from 'components/Layout/CardBase'
 
 export const NoTrove: React.FC = props => {
     const { dispatchEvent } = useTroveView()
@@ -11,33 +12,17 @@ export const NoTrove: React.FC = props => {
     }, [dispatchEvent])
 
     return (
-        <Flex
-            w='555px'
-            height='432px'
-            alignItems='center'
-            justifyContent='center'
-        >
-            <Box
-                w='100%'
-                h='100%'
-                maxW='md'
-                borderWidth={1}
-                borderRadius='31px'
-                overflow='hidden'
-                bg='#131313'
-                color='#FFFFFF'
-            >
-                <Heading>Trove</Heading>
-                <Box sx={{ p: [2, 3] }}>
-                    <InfoMessage title="You haven't borrowed any LUSD yet.">
-                        You can borrow LUSD by opening a Trove.
-                    </InfoMessage>
+        <CardBase>
+            <Heading>Trove</Heading>
+            <Box sx={{ p: [2, 3] }}>
+                <InfoMessage title="You haven't borrowed any LUSD yet.">
+                    You can borrow LUSD by opening a Trove.
+                </InfoMessage>
 
-                    <Flex variant='layout.actions'>
-                        <Button onClick={handleOpenTrove}>Open Trove</Button>
-                    </Flex>
-                </Box>
+                <Flex variant='layout.actions'>
+                    <Button onClick={handleOpenTrove}>Open Trove</Button>
+                </Flex>
             </Box>
-        </Flex>
+        </CardBase>
     )
 }
