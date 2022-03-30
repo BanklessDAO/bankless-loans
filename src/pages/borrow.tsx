7c9e7ef08591a7d3e716729f07e509e7ac396930
Birdman <geositta@protonmail.com>
2022-03-30 18:40:46 -0500

    Add landing to pages and routing to navbar

---
+++ src/pages/borrow.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Wallet } from '@ethersproject/wallet'
+import { Decimal, Difference, Trove } from '@liquity/lib-base'
+import { LiquityStoreProvider } from '../components/LiquityStoreProvider'
+import { useLiquity } from '../hooks/LiquityContext'
+import { TransactionMonitor } from '../components/Transaction'
+import { VStackBase } from '../components/Layout/VStackBase'
+import { Trove as TroveEditor } from '../components/Trove/Trove'
+import { TroveViewProvider } from '../components/Trove/context/TroveViewProvider'
+
+type LiquityFrontendProps = {
+    loader?: React.ReactNode
+}
+const Borrow = ({ loader }: LiquityFrontendProps): JSX.Element => {
+    const { account, provider, liquity } = useLiquity()
+
+    // For console tinkering ;-)
+    Object.assign(window, {
+        account,
+        provider,
+        liquity,
+        Trove,
+        Decimal,
+        Difference,
+        Wallet,
+    })
+
+    return (
+        <LiquityStoreProvider {...{ loader }} store={liquity.store}>
+            <TroveViewProvider>
+                <VStackBase>
+                    <TroveEditor />
+                </VStackBase>
+            </TroveViewProvider>
+            <TransactionMonitor />
+        </LiquityStoreProvider>
+    )
+}
+
+export default Borrow
