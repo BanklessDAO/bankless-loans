11319a5d5d6971b6aea6100ac9c39b43ccd80855
Birdman <geositta@protonmail.com>
2022-04-04 18:48:47 -0500

    Feat - Add glass cards to landing

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+    HStack,
     Box,
     Text,
     Flex,
@@ -8,31 +9,45 @@ import {
     GlobalStyle,
     LightMode,
 } from '@chakra-ui/react'
+import { SoftCard } from '../components/Layout/SoftCard'
+import { Lifebuoy, PalmUp, Blocks } from '../components/LandingLogos'
+import { Icon } from '@chakra-ui/react'
 
 export default function Landing() {
     return (
-        <Box>
+        <Box h='100vh'>
             <LightMode>
                 <GlobalStyle />
                 <Flex direction='column'>
-                    <Heading color='#FFFFFF'>Become Bankless</Heading>
+                    <Heading color='#FFFFFF'>Become Bankless,</Heading>
                     <Heading color='#FFFFFF'>
-                        Take out an interest-free loan
+                        take out an interest-free loan
                     </Heading>
                 </Flex>
                 <Box mt='3' p='5'>
-                    <Stack
-                        mt='3'
-                        justifyContent='space-around'
-                        direction={['column', 'row']}
-                    >
-                        <Box>
-                            <Text as='b' fontSize='sm'>
-                                title
-                            </Text>
-                            <Text fontSize='xs'>descirption</Text>
-                        </Box>
-                    </Stack>
+                    <HStack>
+                        <SoftCard
+                            iconBoxColor='#6D29FE'
+                            icon={<Icon as={PalmUp} />}
+                            title='Borrow'
+                            header='Deposit ETH'
+                            body='Borrow LUSD at 0% interest'
+                        />
+                        <SoftCard
+                            iconBoxColor='#BE04FF'
+                            icon={<Icon as={Blocks} />}
+                            title='Pool'
+                            header='Deposit LUSD'
+                            body='Earn ETH & LQTY'
+                        />
+                        <SoftCard
+                            iconBoxColor='#00FFA3'
+                            icon={<Icon as={Lifebuoy} />}
+                            title='Stake'
+                            header='Stake LQTY'
+                            body='Earn LUSD & ETH'
+                        />
+                    </HStack>
                 </Box>
             </LightMode>
         </Box>
