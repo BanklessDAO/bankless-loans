56dc58a0a7630863084a926c36f26865a0bc3dc5
Birdman <geositta@protonmail.com>
2022-04-04 22:06:49 -0500

    Feature - Rename to GlassCard

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,55 +1,72 @@
 import React from 'react'
 import {
-    HStack,
+    Container,
+    VStack,
+    Wrap,
+    WrapItem,
     Box,
     Text,
-    Flex,
-    Heading,
     Stack,
     GlobalStyle,
     LightMode,
 } from '@chakra-ui/react'
-import { SoftCard } from '../components/Layout/SoftCard'
+import { GlassCard } from '../components/Layout/GlassCard'
 import { Lifebuoy, PalmUp, Blocks } from '../components/LandingLogos'
 import { Icon } from '@chakra-ui/react'
 
 export default function Landing() {
     return (
-        <Box h='100vh'>
-            <LightMode>
-                <GlobalStyle />
-                <Flex direction='column'>
-                    <Heading color='#FFFFFF'>Become Bankless,</Heading>
-                    <Heading color='#FFFFFF'>
-                        take out an interest-free loan
-                    </Heading>
-                </Flex>
-                <Box mt='3' p='5'>
-                    <HStack>
-                        <SoftCard
-                            iconBoxColor='#6D29FE'
-                            icon={<Icon as={PalmUp} />}
-                            title='Borrow'
-                            header='Deposit ETH'
-                            body='Borrow LUSD at 0% interest'
-                        />
-                        <SoftCard
-                            iconBoxColor='#BE04FF'
-                            icon={<Icon as={Blocks} />}
-                            title='Pool'
-                            header='Deposit LUSD'
-                            body='Earn ETH & LQTY'
-                        />
-                        <SoftCard
-                            iconBoxColor='#00FFA3'
-                            icon={<Icon as={Lifebuoy} />}
-                            title='Stake'
-                            header='Stake LQTY'
-                            body='Earn LUSD & ETH'
-                        />
-                    </HStack>
-                </Box>
-            </LightMode>
-        </Box>
+        <Container as='section' maxW='6xl' pt={[4, 8]} px={[4, 8]}>
+            <Stack spacing={[4, 8]}>
+                <LightMode>
+                    <GlobalStyle />
+                    <Text variant='home-title'>
+                        Become Bankless, take out an interest-free loan
+                    </Text>
+                    <Text variant='home-subtitle'>
+                        Borrow, Earn and Stake with the fully decentralised
+                        Bankless DAO App, built on the Liquity protocol.
+                    </Text>
+                    <Box as='section'>
+                        <Container
+                            as={VStack}
+                            maxW='6xl'
+                            px={[4, 8]}
+                            spacing={[8, 12]}
+                        >
+                            <Wrap justify='center' spacing={[4, 8]}>
+                                <WrapItem>
+                                    <GlassCard
+                                        iconBoxColor='#6D29FE'
+                                        icon={<Icon as={PalmUp} />}
+                                        title='Borrow'
+                                        header='Deposit ETH'
+                                        body='Borrow LUSD at 0% interest'
+                                    />
+                                </WrapItem>
+                                <WrapItem>
+                                    <GlassCard
+                                        iconBoxColor='#BE04FF'
+                                        icon={<Icon as={Blocks} />}
+                                        title='Pool'
+                                        header='Deposit LUSD'
+                                        body='Earn ETH & LQTY'
+                                    />
+                                </WrapItem>
+                                <WrapItem>
+                                    <GlassCard
+                                        iconBoxColor='#00FFA3'
+                                        icon={<Icon as={Lifebuoy} />}
+                                        title='Stake'
+                                        header='Stake LQTY'
+                                        body='Earn LUSD & ETH'
+                                    />
+                                </WrapItem>
+                            </Wrap>
+                        </Container>
+                    </Box>
+                </LightMode>
+            </Stack>
+        </Container>
     )
 }
