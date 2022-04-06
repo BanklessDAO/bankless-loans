22c52049dc14d358e49244f59f73b0c66e6c41e5
Birdman <geositta@protonmail.com>
2022-04-05 22:35:44 -0500

    Fix - Add CardBase to preview components

--- src/components/Stability/StabilityPreview.tsx
+++ src/components/Stability/StabilityPreview.tsx
@@ -3,46 +3,31 @@ import { Flex, Box, Heading } from '@chakra-ui/react'
 import { WalletConnector } from '../WalletConnector'
 import { DisabledEditableRow } from '../Trove/Editor'
 import { useModal } from 'hooks/ModalContext'
+import { CardBase } from '../Layout/CardBase'
 
 export const StabilityPreview = (): JSX.Element => {
     const modal = useModal()
     //will need to address hard-coded width for mobile
     return (
-        <Flex
-            w='555px'
-            height='400px'
-            alignItems='center'
-            justifyContent='center'
-        >
-            <Box
-                w='100%'
-                h='100%'
-                maxW='md'
-                borderRadius='31px'
-                overflow='hidden'
-                padding={[10, 34, 34, 5]}
-                bg='#131313'
-                color='#FFFFFF'
-            >
-                <Heading>Stability Pool</Heading>
+        <CardBase>
+            <Heading>Stability Pool</Heading>
 
-                <DisabledEditableRow
-                    label='Deposit'
-                    inputID='stake-lqty'
-                    amount={'0.000'}
-                    unit={'LUSD'}
+            <DisabledEditableRow
+                label='Deposit'
+                inputID='stake-lqty'
+                amount={'0.000'}
+                unit={'LUSD'}
+            />
+
+            <Box ml='10px' w='full' h='100px'>
+                <h3>Please connect your wallet to use our services</h3>
+                <br />
+                <WalletConnector
+                    isOpen={modal.isModalOpen}
+                    onOpen={modal.openModal}
+                    onClose={modal.closeModal}
                 />
-
-                <Box ml='10px' w='full' h='100px'>
-                    <h3>Please connect your wallet to use our services</h3>
-                    <br />
-                    <WalletConnector
-                        isOpen={modal.isModalOpen}
-                        onOpen={modal.openModal}
-                        onClose={modal.closeModal}
-                    />
-                </Box>
             </Box>
-        </Flex>
+        </CardBase>
     )
 }
