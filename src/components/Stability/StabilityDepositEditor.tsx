c6b5e7f958b35b79387d22af590ff61aa7e4f831
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-04-12 02:28:21 +0100

    STYLES (stability/deposit) : update sthe deposit and adjusting view to new design
    Uses new input components

--- src/components/Stability/StabilityDepositEditor.tsx
+++ src/components/Stability/StabilityDepositEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Flex, Heading, Box, Button } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
 
 import {
     Decimal,
@@ -14,10 +14,11 @@ import { useLiquitySelector } from '../../hooks/useLiquitySelector'
 import { COIN, GT } from '../../strings'
 
 import { Icon } from '../Icon'
-import { EditableRow, StaticRow } from '../Trove/Editor'
+import { EditableRow, StaticRowV2 } from '../Trove/Editor'
 import { LoadingOverlay } from '../LoadingOverlay'
 import { InfoIcon } from '../InfoIcon'
 import { CardBase } from 'components/Layout/CardBase'
+import { HeadingBase } from 'components/HeadingBase'
 
 const select = ({ lusdBalance, lusdInStabilityPool }: LiquityStoreState) => ({
     lusdBalance,
@@ -65,7 +66,7 @@ export const StabilityDepositEditor: React.FC<StabilityDepositEditorProps> = ({
 
     return (
         <CardBase>
-            <Heading>
+            <HeadingBase>
                 Stability Pool
                 {edited && !changePending && (
                     <Button
@@ -76,9 +77,9 @@ export const StabilityDepositEditor: React.FC<StabilityDepositEditorProps> = ({
                         <Icon name='history' size='lg' />
                     </Button>
                 )}
-            </Heading>
+            </HeadingBase>
 
-            <Box sx={{ p: [2, 3] }}>
+            <Box>
                 <EditableRow
                     label='Deposit'
                     inputID='deposit-lqty'
@@ -92,29 +93,32 @@ export const StabilityDepositEditor: React.FC<StabilityDepositEditorProps> = ({
                         dispatch({ type: 'setDeposit', newValue })
                     }
                 />
-
-                {newPoolShare.infinite ? (
-                    <StaticRow
-                        label='Pool share'
-                        inputID='deposit-share'
-                        amount='N/A'
-                    />
-                ) : (
-                    <StaticRow
-                        label='Pool share'
-                        inputID='deposit-share'
-                        amount={newPoolShare.prettify(4)}
-                        pendingAmount={poolShareChange?.prettify(4).concat('%')}
-                        pendingColor={
-                            poolShareChange?.positive ? 'success' : 'danger'
-                        }
-                        unit='%'
-                    />
-                )}
+                <Box my={4}>
+                    {newPoolShare.infinite ? (
+                        <StaticRowV2
+                            label='Pool share'
+                            inputID='deposit-share'
+                            amount='N/A'
+                        />
+                    ) : (
+                        <StaticRowV2
+                            label='Pool share'
+                            inputID='deposit-share'
+                            amount={newPoolShare.prettify(4)}
+                            pendingAmount={poolShareChange
+                                ?.prettify(4)
+                                .concat('%')}
+                            pendingColor={
+                                poolShareChange?.positive ? 'success' : 'danger'
+                            }
+                            unit='%'
+                        />
+                    )}
+                </Box>
 
                 {!originalDeposit.isEmpty && (
                     <>
-                        <StaticRow
+                        <StaticRowV2
                             label='Liquidation gain'
                             inputID='deposit-gain'
                             amount={originalDeposit.collateralGain.prettify(4)}
@@ -125,7 +129,7 @@ export const StabilityDepositEditor: React.FC<StabilityDepositEditorProps> = ({
                             unit='ETH'
                         />
 
-                        <StaticRow
+                        <StaticRowV2
                             label='Reward'
                             inputID='deposit-reward'
                             amount={originalDeposit.lqtyReward.prettify()}
@@ -133,32 +137,7 @@ export const StabilityDepositEditor: React.FC<StabilityDepositEditorProps> = ({
                                 originalDeposit.lqtyReward.nonZero && 'success'
                             }
                             unit={GT}
-                            infoIcon={
-                                <InfoIcon
-                                    tooltip={
-                                        <Box
-                                            sx={{
-                                                padding: '10px',
-                                                bg: '#a7a7e3',
-                                                fontSize: '1em',
-                                                color: '#333',
-                                                minW: '12.5vw',
-                                                height: 'auto',
-                                                maxW: '33vm',
-                                                fontStyle: 'italic',
-                                            }}
-                                        >
-                                            Although the LQTY rewards accrue
-                                            every minute, the value on the UI
-                                            only updates when a user transacts
-                                            with the Stability Pool. Therefore
-                                            you may receive more rewards than is
-                                            displayed when you claim or adjust
-                                            your deposit.
-                                        </Box>
-                                    }
-                                />
-                            }
+                            tooltipText='Although the LQTY rewards accrue every minute, the value on the UI only updates when a user transacts with the Stability Pool. Therefore you may receive more rewards than is displayed when you claim or adjust your deposit.'
                         />
                     </>
                 )}
