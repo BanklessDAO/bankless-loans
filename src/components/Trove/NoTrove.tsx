7c858a6af3d2b0aa60a6a6f335d7b698773c8c57
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-04-12 01:29:49 +0100

    STYLE (trove/none) : updates the none panel view to match the design

--- src/components/Trove/NoTrove.tsx
+++ src/components/Trove/NoTrove.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback } from 'react'
-import { Heading, Box, Flex, Button } from '@chakra-ui/react'
+import { Heading, Box, Flex, Button, HStack } from '@chakra-ui/react'
 import { InfoMessage } from '../InfoMessage'
 import { useTroveView } from './context/TroveViewContext'
 import { CardBase } from 'components/Layout/CardBase'
+import { ActionDescriptionV2 } from 'components/ActionDescriptionV2'
 
 export const NoTrove: React.FC = props => {
     const { dispatchEvent } = useTroveView()
@@ -13,17 +14,20 @@ export const NoTrove: React.FC = props => {
 
     return (
         <CardBase>
-            <Heading>Trove</Heading>
-            <Box sx={{ p: [2, 3] }}>
-                <InfoMessage title="You haven't borrowed any LUSD yet.">
-                    You can borrow LUSD by opening a Trove.
-                </InfoMessage>
-
-                <Flex>
-                    <Button variant='mainPurple' onClick={handleOpenTrove}>
+            <Heading marginBottom={18}>Trove</Heading>
+            <Box>
+                <ActionDescriptionV2>
+                    {`You haven't borrowed any LUSD yet. You can borrow LUSD by opening a Trove.`}
+                </ActionDescriptionV2>
+                <HStack>
+                    <Button
+                        variant='mainPurple'
+                        onClick={handleOpenTrove}
+                        margin='0'
+                    >
                         Open Trove
                     </Button>
-                </Flex>
+                </HStack>
             </Box>
         </CardBase>
     )
