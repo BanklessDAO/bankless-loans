4c85a9079264b36d5d790b19e9daeb198b98af18
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2022-04-13 02:43:30 +0100

    FIX (styles.ts) : adds back in the mode() fn

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -15,9 +15,8 @@ const styles = {
             fontSize: '18px',
         },
         body: {
-            backgroundColor: '#000000',
-            background: `radial-gradient(circle at left, rgba(109, 41, 254, 0.3) 0%, rgba(109, 41, 254, 0) 30%),
-            radial-gradient(circle at right, rgba(255, 4, 16, 0.24) 0%, rgba(255, 4, 16, 0) 30%)`,
+            background: mode(gradientAndImage, gradient)(props),
+            //backgroundSize: '100% auto',
         },
         tippyPopper: {
             bg: '#ff0000',
