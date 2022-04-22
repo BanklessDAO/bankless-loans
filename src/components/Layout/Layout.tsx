e8216b7d513e4240d2444501499b1e1cee03806f
Birdman <geositta@protonmail.com>
2022-04-21 22:29:37 -0500

    Fix - Refactor background style

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -8,12 +8,22 @@ type LayoutProps = {
     children?: React.ReactNode
 }
 
+const gradient = `radial-gradient(circle at left, rgba(109, 41, 254, 0.3) 0%,
+    rgba(109, 41, 254, 0) 30%), radial-gradient(circle at right,
+    rgba(255, 4, 16, 0.24) 0%, rgba(255, 4, 16, 0) 30%), #000000`
+
+const gradientAndImage = `radial-gradient(circle at left, rgba(109, 41, 254, 0.3) 0%,
+    rgba(109, 41, 254, 0) 30%), radial-gradient(circle at right,
+    rgba(255, 4, 16, 0.24) 0%, rgba(255, 4, 16, 0) 30%),
+    center top url(/futuristic-background.png), #000000`
+
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
     const router = useRouter()
     const footer = router.pathname === '/' && <Footer />
+    const bg = router.pathname === '/' ? gradientAndImage : gradient
     return (
-        <Container maxW='container.xl' p={0}>
-            <VStack align='center' as='main' position='relative' height='100vh'>
+        <Container bg={bg} maxW='full' p={0}>
+            <VStack align='center' as='main' position='relative'>
                 <Navbar />
                 {children}
                 {footer}
